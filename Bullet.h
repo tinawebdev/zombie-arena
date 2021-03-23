a798e2de5a37ecd4795e4bb252c3dcc2b4a206d5
Kristina Goncharova <codingkris@yahoo.com>
2021-03-23 16:46:16 +0300

    Add the Bullet header file

---
+++ Bullet.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Bullet
+{
+private:
+    // Where is the bullet?
+    Vector2f m_Position;
+
+    // What each bullet looks like
+    RectangleShape m_BulletShape;
+
+    // Is this bullet currently whizzing through the air
+    bool m_InFlight = false;
+
+    // How fast does a bullet travel?
+    float m_BulletSpeed = 1000;
+
+    // What is fraction of 1 pixel does the bullet travel,
+    // Horizontally and vertically each frame?
+    // These values will be derived from m_BulletSpeed
+    float m_BulletDistanceX;
+    float m_BulletDistanceY;
+
+    // Some boundaries so the bullet doesn't fly forever
+    float m_MaxX;
+    float m_MinX;
+    float m_MaxY;
+    float m_MinY;
+
+public:
+    Bullet();
+
+    // Stop the bullet
+    void stop();
+
+    // Returns the value of m_InFlight
+    bool isInFlight();
+
+    // Launch a new bullet
+    void shoot(float startX, float startY,
+        float xTarget, float yTarget);
+
+    // Where the bullet is in the world
+    FloatRect getPosition();
+
+    // Return the actual shape (for drawing)
+    RectangleShape getShape();
+
+    // Update the bullet each frame
+    void update(float elapsedTime);
+};
