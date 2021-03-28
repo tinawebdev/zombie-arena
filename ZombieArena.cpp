3a6757bcad3f3f4e68ecf9f7f23a2b4f693fba03
Kristina Goncharova <codingkris@yahoo.com>
2021-03-28 17:18:31 +0300

    Giving the player a crosshair

--- ZombieArena.cpp
+++ ZombieArena.cpp
@@ -68,6 +68,13 @@ int main()
     // When was the fire button last pressed?
     Time lastPressed;
 
+    // Hide the mouse pointer and replace it with crosshair
+    window.setMouseCursorVisible(true);
+    Sprite spriteCrosshair;
+    Texture textureCrosshair = TextureHolder::GetTexture("graphics/crosshair.png");
+    spriteCrosshair.setTexture(textureCrosshair);
+    spriteCrosshair.setOrigin(25, 25);
+
     // The main game loop
     while (window.isOpen())
     {
@@ -291,6 +298,9 @@ int main()
             mouseWorldPosition = window.mapPixelToCoords(
                 Mouse::getPosition(), mainView);
 
+            // Set the crosshair to the mouse world location
+            spriteCrosshair.setPosition(mouseWorldPosition);
+
             // Update the player
             player.update(dtAsSeconds, Mouse::getPosition());
 
@@ -350,6 +360,9 @@ int main()
 
             // Draw the player
             window.draw(player.getSprite());
+
+            // Draw the crosshair
+            window.draw(spriteCrosshair);
         }
 
         if (state == State::LEVELING_UP)
